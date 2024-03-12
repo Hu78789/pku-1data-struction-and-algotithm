5d9599a7c29bfcbcda751c3905d4ac7ea0c2f0ad
Hu78789 <160581378+Hu78789@users.noreply.github.com>
2024-03-12 16:54:57 +0800

    Update tree.py

--- tree.py
+++ tree.py
@@ -12,6 +12,12 @@ def count_leaves(node):
     if node.left is None and node.right is None:
         return 1
     return count_leaves(node.left) + count_leaves(node.right)
+def tree_depth(node):
+    if node is None:
+        return 0
+    left_depth = tree_depth(node.left)
+    right_depth = tree_depth(node.right)
+    return max(left_depth,right_depth) + 1    
 n = int(input())
 nodes = [TreeNode() for _ in range(n)]
 has_parent = [False] * n
