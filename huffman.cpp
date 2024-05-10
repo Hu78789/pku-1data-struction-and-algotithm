60e60de16b4bd5da3a4a326b15c4875bcdd1cb10
Hu78789 <160581378+Hu78789@users.noreply.github.com>
2024-05-11 00:26:39 +0800

    Update huffman.cpp

--- huffman.cpp
+++ huffman.cpp
@@ -1,58 +1,80 @@
-#include <iostream>
-#include <string>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#include <map>
-#include <iterator>
+#include<bits/stdc++.h>
 using namespace std;
-typedef vector<char> HuffmanCode;
-typedef map<char, HuffmanCode> HuffmanTable;
-class INode{
-    public:
-        const int freq;
-        virtual ~INode(){}
-    protected:
-        INode(int f):freq(f){}
+struct HuffmanNode{
+    char data;
+    int freq;
+    HuffmanNode *left;
+    HuffmanNode *right;
+    HuffmanNode(char data,int freq): data(data),freq(freq),left(nullptr),right(nullptr){}
 };
-class InternalNode: public INode
-{
-    public:    
-        INode* left;
-        INode* right;
-        InternalNode(INode* l, INode* r):
-            INode(l->freq + r->freq), left(l), right(r){}
-            ~InternalNode(){
-                delete left;
-                delete right;
-            }
-            
-};
-class LeafNode: public INode{
-    public:
-        const char c;
-        LeafNode(int f,char c):INode(f),c(c){}
-};
-struct NodeCmp{
-    bool operator()(const INode* lhs,const INode* rhs) const{return lhs->freq > rhs->freq;}
-};
-INode* BuildTree(map<char,int> frequs){
-    priority_queue<INode*, vector<INode*>, NodeCmp> trees;
-    map<char,int>::iterator it = frequs.begin();
-    for (; it != frequs.end();it++){
-        trees.push(new LeafNode(it->second,it->first));
+struct CmpNodes{
+    bool operator()(HuffmanNode*a,HuffmanNode*b){
+        return a->freq > b->freq;
     }
-    while (trees.size() > 1){
-        INode* childR = trees.top();
-        trees.pop();
-        INode* childL = trees.top();
-        trees.pop();
-        trees.push(new InternalNode(childL,childR));
-    }
-    return trees.top();
 };
-void GenerateCode(const INode* node,const HuffmanTable& prefix, HuffmanCode& outCodes){
-    
+HuffmanNode* buildHuffmanTree(const unordered_map<char,int>&freqMap){
+    priority_queue<HuffmanNode*,vector<HuffmanNode*>,CmpNodes> pq;
+    for (const auto& pair : freqMap){
+        pq.push(new HuffmanNode(pair.first,pair.second));
+        }
+    while (pq.size()>1){
+        HuffmanNode* left = pq.top();pq.pop();
+        HuffmanNode* right = pq.top();pq.pop();
+        HuffmanNode* newNode = new HuffmanNode('$',left->freq+right->freq);
+        newNode->left = left;
+        newNode->right = right;
+        pq.push(newNode);
+    }
+    return pq.top();    
+}
+void generateHuffmanCode(HuffmanNode* root,string code,unordered_map<char,string>& huffmanCodes){
+    if (root == nullptr) return;
+    if (!root->left && !root->right){
+        huffmanCodes[root->data] = code;
+    }
+    generateHuffmanCode(root->left,code+"0",huffmanCodes);
+    generateHuffmanCode(root->right,code + "1",huffmanCodes);
+}
+unordered_map<char,string> huffmanEncoding(const unordered_map<char,int>&freqMap){
+    HuffmanNode* root = buildHuffmanTree(freqMap);
+    unordered_map<char,string> huffmanCodes;
+    generateHuffmanCode(root,"",huffmanCodes);
+    return huffmanCodes;
+}
+string decodeHuffmanData(HuffmanNode *root,const string& str){
+    string ans = "";
+    HuffmanNode*curr = root;
+    for (char s:str){
+        if (s == '0'){
+            curr = curr->left;    
+        }else curr = curr->right;
+    }
+    if(curr->left == nullptr && curr->right == nullptr){
+        ans += curr->data;
+        curr = root;
+    }
+    return ans;
 }
 
 
+int main() {
+    // 测试字符频率
+    unordered_map<char, int> freqMap = {
+        {'a', 5}, {'b', 9}, {'c', 12}, {'d', 13},
+        {'e', 16}, {'f', 45}
+    };
+
+    // 生成哈夫曼编码
+    unordered_map<char, string> huffmanCodes = huffmanEncoding(freqMap);
+
+    // 输出结果
+    cout<<"Huffman Codes:\n";
+    for(const auto& pair : huffmanCodes) {
+        cout << pair.first << " : " << pair.second << endl;
+    }
+    string a="0";
+    string result = decodeHuffmanData(buildHuffmanTree(freqMap),a);
+    cout<<result<<endl;
+
+    return 0;
+}
